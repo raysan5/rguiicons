35ada0efb3da9a040ba4aee36e6571129b84b681
Ray <raysan5@gmail.com>
2025-09-03 11:06:41 +0200

    Update external libraries and section comments

--- src/rguiicons.c
+++ src/rguiicons.c
@@ -1310,7 +1310,7 @@ int main(int argc, char *argv[])
 }
 
 //--------------------------------------------------------------------------------------------
-// Module functions definition
+// Module Functions Definition
 //--------------------------------------------------------------------------------------------
 #if defined(PLATFORM_DESKTOP)
 // Show command line usage info
